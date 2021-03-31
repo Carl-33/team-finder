be86fbc207f99df7e0aa9eecf1c98f4d8658e438
carl-33 <koepkecj@gmail.com>
2021-03-31 18:15:24 -0500

    updated button styles, and when you click on a button it brings up basic team info, and roster

--- assets/script.js
+++ assets/script.js
@@ -14,7 +14,7 @@ $(document).ready(function () {
         for (let i = 0; i < 31; i++) {
             let teamsList = $("<button>")
             teamsList.text(teamsData.teams[i].name)
-            teamsList.addClass("team-button")
+            teamsList.addClass("btn btn-outline-light team-button ")
             teamsList.attr("id", teamsData.teams[i].id)
             buttonsHere.append(teamsList)
         }
@@ -31,21 +31,58 @@ $(document).ready(function () {
         $("#teamInfoBox").empty();
         console.log("copyright: " + teamInfoData.copyright)
         console.log("team info data " + teamInfoData.teams[0].name)
-        let teamDisplay = $("<h1>")
-        teamDisplay.text(teamInfoData.teams[0].name)
-        $("#teamInfoBox").append(teamDisplay)
+        let teamName = $("<h1>").text(teamInfoData.teams[0].name)
+        $("#teamInfoBox").append(teamName)
+        let teamWebsite = $("<a>").text(teamInfoData.teams[0].officialSiteUrl).attr({
+            href: teamInfoData.teams[0].officialSiteUrl, 
+            target: "_blank"
+        })
+        $("#teamInfoBox").append(teamWebsite)
+        let foundedYear = $("<p>").text("Founded: " + teamInfoData.teams[0].firstYearOfPlay)
+        $("#teamInfoBox").append(foundedYear)
     }
     function getTeamPlayers (teamId) {
         let playerQuery = "https://statsapi.web.nhl.com/api/v1/teams/" + teamId + "/roster";
         $.ajax({
             url: playerQuery,
             method: "GET"
-        // }).then(displayPlayerInfo)
-        }).then(function (res) {
-            console.log(playerQuery)
-            console.log(res)
-            console.log(res.roster[0].person.fullName)
-        })  
+        }).then(displayPlayerInfo)  
+    }
+    function displayPlayerInfo (playerInfo) {
+        console.log(playerInfo)
+        let rosterHeader = $("<h5>").text("Team Roster")
+        $('#teamInfoBox').append(rosterHeader)
+        // Table is not working yet
+        // let tableSpot = $("#tableHere")
+        // let rosterTable = $("<table>").attr("id", "rosterTable")
+        // let rosterTableRow = $("<tr>")
+        // let rosterHeaderName = $("<th>").text("Name")
+        // let rosterHeaderNumber = $("<th>").text("Number")
+        // let rosterHeaderPosition = $("<th>").text("Position")
+        // let rosterTableRowEnd = $("</tr>")
+        // tableSpot.append(rosterTable)
+        // tableSpot.append(rosterTableRow)
+        // tableSpot.append(rosterHeaderName)
+        // tableSpot.append(rosterHeaderNumber)
+        // tableSpot.append(rosterHeaderPosition)
+        // tableSpot.append(rosterTableRowEnd)
+        for (let i = 0; i < playerInfo.roster.length; i++) {
+            console.log(playerInfo.roster[i].person.fullName)
+            let playerStuff = $('<p>').text("Name: " + playerInfo.roster[i].person.fullName + 
+                " Number: " + playerInfo.roster[i].jerseyNumber + 
+                " Position: " +  playerInfo.roster[i].position.name)
+            $('#teamInfoBox').append(playerStuff)
+            // Table is not working yet
+            // let playerName = $("<td>").text(playerInfo.roster[i].person.fullName)
+            // let playerNumber = $("<td>").text(playerInfo.roster[i].person.jerseyNumber)
+            // let playerPosition = $("<td>").text(playerInfo.roster[i].position.name)
+            // tableSpot.append(rosterTableRow);
+            // tableSpot.append(playerName);
+            // tableSpot.append(playerNumber);
+            // tableSpot.append(playerPosition); 
+            // tableSpot.append(rosterTableRow);
+        }
+        // $("#tableHere").append(rosterTable)
     }
     $(document).on("click", ".team-button", function() { 
         let teamId = $(this).attr('id');
